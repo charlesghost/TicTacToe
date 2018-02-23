a3793739757690709b0d40988762eaec21231eba
Tech-A10 <tech-a10@MacBook-Air-8.local>
2018-02-23 05:15:32 +0000

    fixed the x's and o's issues

--- script.js
+++ script.js
@@ -1,56 +1,30 @@
 $(function(event){
 
 
-	
+	var move = 1;
+	var play = true;
+
+
+	$("td").click(function() {
+		if ($(this).text()=="" && play) {
+			if ((move%2)==1) { 
+				$(this).append("X"); 
+				$(".playerTurn").text("It is 0's turn");
+			} else { 
+				$(this).append("O"); 
+				$(".playerTurn").text("It is X's turn");
+			}
+			move++; 
+		}
+
+	});
+
 
 	$("#reset").on("click", function(event){
 		$("td").text('');
 		$(".playerTurn").text("It is X's turn");
 	});
 
-	
-
-	$("td").click(function(event){
-	//	$(this).text('X');
-
-		if($("this").text('')){
-				$(this).text("X");
-				$(".playerTurn").text("It is 0's turn");
-			}else if($(this).text('X')) {
-				$(this).text("0");
-			}
-		});
-
-		
-
-	// var X = $("td").text('X');
-
-	// var Y = $("td").text('Y');
-
-	 $("table").click(function(){
-        if ($(this).text()==""){
-        	console.log("X");
-        }
-
-	 })
-
-/*
-
-	$("td").click(function() {
-		if ($(this).text()=="" && play) {
-			if ((move%2)==1) { 
-				$(this).append("X"); } 
-				else { $(this).append("O"); }
-      
-      }
-    });
-
-    */
   
 	
-
-
-
-
-
 });
